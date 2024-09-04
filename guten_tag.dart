a8835c984dfa7c1afced5defe28beafab48e53a6
Sascha Ziegler <sascha8398@googlemail.com>
2024-09-04 11:26:18 +0200

    added guten_tag.dart

---
+++ guten_tag.dart
@@ -0,0 +1,22 @@
+/*
+Schreibe eine Klasse, die mit einem Namen instanziiert wird 
+und eine Methode hat, die den Namen mit "Guten Tag, " davor auf der Konsole ausgibt. 
+Schreibe eine Methode, die den Namen auf einen anderen Namen setzt.
+*/
+
+class GoodDay {
+  String name;
+
+  GoodDay(this.name);
+
+  void sayGoodDay() {
+    print("Guten Tag, $name");
+  }
+}
+
+void main() {
+  GoodDay name1 = GoodDay("Sascha");
+  name1.sayGoodDay();
+  name1.name = "Kai";
+  name1.sayGoodDay();
+}
