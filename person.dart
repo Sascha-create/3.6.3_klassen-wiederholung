37c84f40b418ca7739d235dc87456cdddb258e3e
Sascha Ziegler <sascha8398@googlemail.com>
2024-09-04 13:50:09 +0200

    person fertig

--- person.dart
+++ person.dart
@@ -2,4 +2,32 @@ class Person {
   String name;
   int age;
   Adress personsAdress;
+
+  Person(this.name, this.age, this.personsAdress);
+
+  String newName(String newName) {
+    return name = newName;
+  }
+
+  int newAge(int newAge) {
+    return age = newAge;
+  }
+}
+
+class Adress {
+  String streetName;
+  int houseNumber;
+  int zipCode;
+  String location;
+
+  Adress(this.streetName, this.houseNumber, this.zipCode, this.location);
+}
+
+void main() {
+  Adress kaisAdress = Adress("Dartstrasse", 5, 76448, "Darthausen");
+  Person kai = Person("Kai Hatje", 22, kaisAdress);
+
+  print("${kai.age}");
+  kai.newAge(55);
+  print("${kai.age}");
 }
